d52b383bd255642602ef29b577d144200760bdd0
richxlieu2003 <newriche08@gmail.com>
2026-02-16 15:46:58 +0100

    Voici le code sans le bonus

--- age_calculator.py
+++ age_calculator.py
@@ -1,11 +1,11 @@
 import datetime
 
 def main():
-    birth_year = 0  
+    birth_year = int(input("Enter your birth year: ")) 
 
-    current_year = 0 
+    current_year = datetime.date.today().year
 
-    age = 0
+    age = current_year - birth_year
 
     print("--------------------")
     print(f"Current Year: {current_year}")
